d586fc27a3fc1a444b3d921fe858b3e1ad890421
Steven Rummler <steven.rummler@gmail.com>
2021-12-15 19:26:11 -0700

    Update

--- src/main/java/com/stevenrummler/cosmere_collection/CosmereCollectionClient.java
+++ src/main/java/com/stevenrummler/cosmere_collection/CosmereCollectionClient.java
@@ -1,8 +1,8 @@
 package com.stevenrummler.cosmere_collection;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+//import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+//import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.StickyKeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -14,17 +14,17 @@ public class CosmereCollectionClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        KeyBinding honorblade_power_primary = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.cosmere_collection.honorblade_power_primary", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "key.category.cosmere_collection"));
-        KeyBinding honorblade_power_secondary = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.cosmere_collection.honorblade_power_secondary", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F, "key.category.cosmere_collection"));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (honorblade_power_primary.wasPressed()) {
-                client.player.sendMessage(new LiteralText("Primary" + client.player.getInventory().getMainHandStack().getItem().toString()), false);
-            }
-
-            while (honorblade_power_secondary.wasPressed()) {
-                client.player.sendMessage(new LiteralText("Secondary" + client.player.getInventory().getMainHandStack().getItem().toString()), false);
-            }
-        });
+//        KeyBinding honorblade_power_primary = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.cosmere_collection.honorblade_power_primary", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "key.category.cosmere_collection"));
+//        KeyBinding honorblade_power_secondary = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.cosmere_collection.honorblade_power_secondary", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F, "key.category.cosmere_collection"));
+//
+//        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+//            while (honorblade_power_primary.wasPressed()) {
+//                client.player.sendMessage(new LiteralText("Primary" + client.player.getInventory().getMainHandStack().getItem().toString()), false);
+//            }
+//
+//            while (honorblade_power_secondary.wasPressed()) {
+//                client.player.sendMessage(new LiteralText("Secondary" + client.player.getInventory().getMainHandStack().getItem().toString()), false);
+//            }
+//        });
     }
 }
